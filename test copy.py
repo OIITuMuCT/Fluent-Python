9c14e5e1f8378e41cbfb1f7044183fcdcf3d42d8
OIITuMuCT <vataman@live.ru>
2025-01-17 11:11:04 +0300

    dict

--- test copy.py
+++ test copy.py
@@ -1,9 +1,15 @@
-def print_products(*args):
+# def print_products(*args):
 
-    a = [args[i] for i in range(len(args)) if isinstance(args[i], 1)]
-    
-    for i in range(len(a)):
-        print(f'{i+1}) {a[i]}')
+#     a = [args[i] for i in range(len(args)) if isinstance(args[i], 1)]
+
+#     for i in range(len(a)):
+#         print(f'{i+1}) {a[i]}')
 
 
-print_products("Бананы", [1, 2], ("Stepik",), "Яблоки", "", "Макароны", 5, True)
+# print_products("Бананы", [1, 2], ("Stepik",), "Яблоки", "", "Макароны", 5, True)
+def info_kwargs(**kwargs):
+    for key, value in sorted(kwargs.items()):
+        print(f"{key}: {value}")
+
+
+info_kwargs(first_name="Timur", last_name="Guev", age=28, job="teacher", mobile="920343453872")
